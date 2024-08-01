4af77eca8f7d6e2309452766e882c66647f0e1b5
Serap BALCI <81075168+reset-34@users.noreply.github.com>
2024-08-01 15:44:48 +0300

    Add project files.

---
+++ Klavyeden Kenarlari Girilen Dikdortgenin Alani/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Klavyeden_Kenarlari_Girilen_Dikdortgenin_Alani
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int kisaKenar, uzunKenar, alan;
+
+            Console.WriteLine("dikdörtgenin Kısa Kenarını Girin");
+            kisaKenar = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine("dikdörtgenin Uzun Kenarını Girin");
+           uzunKenar = Int32.Parse(Console.ReadLine());
+
+            alan = kisaKenar * uzunKenar;
+
+            Console.WriteLine("Dikdörtgenin Alanı = " + alan);
+            Console.ReadLine();
+        }
+    }
+}
